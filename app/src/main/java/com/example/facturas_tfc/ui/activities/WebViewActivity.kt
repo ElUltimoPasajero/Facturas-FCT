d22d89ca85f1597c7522511ec1394c917a0baa67
Migue <elultimopasajero87@gmail.com>
2024-04-23 10:08:05 +0200

    Añadida navegaion hacia atras en el WebViewActivity

--- app/src/main/java/com/example/facturas_tfc/ui/activities/WebViewActivity.kt
+++ app/src/main/java/com/example/facturas_tfc/ui/activities/WebViewActivity.kt
@@ -6,8 +6,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.facturas_tfc.R
+import com.example.facturas_tfc.databinding.ActivitySmartSolarBinding
+import com.example.facturas_tfc.databinding.ActivityWebViewBinding
+import com.google.android.material.appbar.MaterialToolbar
 
 class WebViewActivity : AppCompatActivity() {
+
+    private  lateinit var binding : ActivityWebViewBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,6 +22,15 @@ class WebViewActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding = ActivityWebViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val toolbar: MaterialToolbar = binding.includeMaterialToolbarWebView.materialToolbar
+
+
+        toolbar.setNavigationOnClickListener {
+            startActivity(MainActivity.create(this))
         }
     }
 }
