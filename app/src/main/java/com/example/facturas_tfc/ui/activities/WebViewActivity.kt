ba10b1784ba282e429f5ac3850d6b2254a1b1c0e
Migue <elultimopasajero87@gmail.com>
2024-04-23 12:06:36 +0200

    Realizada pantalla de webview

--- app/src/main/java/com/example/facturas_tfc/ui/activities/WebViewActivity.kt
+++ app/src/main/java/com/example/facturas_tfc/ui/activities/WebViewActivity.kt
@@ -7,8 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.facturas_tfc.R
-import com.example.facturas_tfc.databinding.ActivitySmartSolarBinding
 import com.example.facturas_tfc.databinding.ActivityWebViewBinding
 import com.google.android.material.appbar.MaterialToolbar
 
@@ -19,6 +17,7 @@ class WebViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         binding = ActivityWebViewBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -28,22 +27,24 @@ class WebViewActivity : AppCompatActivity() {
             startActivity(MainActivity.create(this))
         }
 
-        val buttonOpenWebView = binding.buttonOpenNavegator
-        val buttonOpenExternalBrowser = binding.buttonOpenNavView
-
-
-        buttonOpenWebView.setOnClickListener {
-            binding.webView.loadUrl("https://www.iberdrola.es")
-        }
-
-        buttonOpenExternalBrowser.setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.iberdrola.es"))
-            startActivity(intent)
-        }
 
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             insets
+        }
+
+        initWebViewButtons()
+
+    }
+
+    private fun initWebViewButtons() {
+        binding.buttonOpenNavegator.setOnClickListener {
+            binding.webView.loadUrl("https://www.iberdrola.es")
+        }
+
+        binding.buttonOpenNavView.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.iberdrola.es"))
+            startActivity(intent)
         }
     }
 }
