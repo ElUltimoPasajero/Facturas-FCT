c0e448c2b2946530699387964913e054c0f61e08
Migue <elultimopasajero87@gmail.com>
2024-04-24 08:06:39 +0200

    Terminada la navegacion en SmartSolarActivity

--- app/src/main/java/com/example/facturas_tfc/ui/activities/SmartSolarActivity.kt
+++ app/src/main/java/com/example/facturas_tfc/ui/activities/SmartSolarActivity.kt
@@ -2,43 +2,73 @@ package com.example.facturas_tfc.ui.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.example.facturas_tfc.R
 import com.example.facturas_tfc.databinding.ActivitySmartSolarBinding
+import com.example.facturas_tfc.ui.fragments.SmartSolarDataFragment
+import com.example.facturas_tfc.ui.fragments.SmartSolarEnergyFragment
+import com.example.facturas_tfc.ui.fragments.SmartSolarInstallationFragment
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.tabs.TabLayout
 
 class SmartSolarActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySmartSolarBinding
+    private lateinit var toolbar: MaterialToolbar
+    private lateinit var tabLayout: TabLayout
+    private lateinit var fragmentContainer: FrameLayout
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_smart_solar)
-
         binding = ActivitySmartSolarBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val toolbar: MaterialToolbar = binding.includeMaterialToolbarSmartSolar.materialToolbar
+
+        toolbar = binding.includeMaterialToolbarSmartSolar.materialToolbar
+        tabLayout = findViewById(R.id.tablayout)
+        fragmentContainer = findViewById(R.id.fragment_container)
+
+        setSupportActionBar(toolbar)
+        replaceFragment(SmartSolarInstallationFragment())
+
+
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                when (tab.position) {
+
+                    0 -> replaceFragment(SmartSolarInstallationFragment())
+                    1 -> replaceFragment(SmartSolarEnergyFragment())
+                    2 -> replaceFragment(SmartSolarDataFragment())
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
 
 
         toolbar.setNavigationOnClickListener {
             startActivity(MainActivity.create(this))
         }
 
-
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_smart_solar)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.activitySmartSolar) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
 
-
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
 }
-
-
 
